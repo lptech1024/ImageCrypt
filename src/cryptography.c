0040e5de13275bb9eca7d23b7033dc217fc4ba4a
Clayton Casciato <lptech1024@gmail.com>
2018-10-13 23:09:01 -0600

    Fixed whitespace.

--- src/cryptography.c
+++ src/cryptography.c
@@ -8,7 +8,7 @@
 bool set_256_bit_key(const char *passphrase, unsigned char *key)
 {
 	// Random, hardcoded salt to make effective use of rainbow tables more difficult/less feasilbe
-   unsigned char salt[] = { 0xEC, 0xBF, 0x99, 0xA0, 0xEE, 0x4B, 0x1D, 0x76, 0x00 };
+	unsigned char salt[] = { 0xEC, 0xBF, 0x99, 0xA0, 0xEE, 0x4B, 0x1D, 0x76, 0x00 };
 
 	return ((bool) PKCS5_PBKDF2_HMAC(passphrase, -1, salt, sizeof(salt) - 1, 100000, EVP_sha512(), (256 / 8), key));
 }
