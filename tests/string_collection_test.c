563b0ed7fd6beab0afec86c0fe62d7fd56733236
Clayton Casciato <lptech1024@gmail.com>
2018-11-03 23:15:48 -0600

    Updated variable names to reflect previous tooling renaming.

--- tests/string_collection_test.c
+++ tests/string_collection_test.c
@@ -6,10 +6,10 @@
 int main()
 {
 	char *end_strings[] = { "test", NULL };
-	string_collection *ecpa = create_string_collection_initial(end_strings);
+	string_collection *end_collection = create_string_collection_initial(end_strings);
 
 	char *test = "test";
-	printf("[%d]\n", string_ends_with_string_collection(test, ecpa));
+	printf("[%d]\n", string_ends_with_string_collection(test, end_collection));
 
-	destroy_string_collection(ecpa);
+	destroy_string_collection(end_collection);
 }
