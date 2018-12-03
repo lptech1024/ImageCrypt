6a7cc25c9cb45a29ff55df2856ad0d13826c961d
Clayton Casciato <lptech1024@gmail.com>
2018-12-02 21:18:07 -0700

    Added tool for returning function result status.

---
+++ src/tools/status.h
@@ -0,0 +1,18 @@
+#ifndef IMAGECRYPT_STATUS
+#define IMAGECRYPT_STATUS
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef enum {
+	SUCCESS = 0,
+	NOT_SUCCESS,
+	ERROR
+} status;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
