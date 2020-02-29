cd01d2d7a2c2839ec5841923cdbd29fd52701666
Clayton Casciato <lptech1024@gmail.com>
2020-02-28 22:55:29 -0700

    Cleaned generic build script.

--- scripts/build_generic.sh
+++ scripts/build_generic.sh
@@ -1,18 +1,15 @@
+#!/bin/bash
+
 # This script is used to build on platforms without custom instructions.
 
-# TODO: Add shebang
-
-# TODO: Check for tools used
-# TODO: Improve structure
-# TODO: Consider adding error codes
-# TODO: Consider adding man page
-
-# TODO: Check command outputs
-
 set -e
-unset IFS
 
-mkdir --parents build
-cd build
-cmake ..
-make package
+main()
+{
+	mkdir --parents build
+	cd build
+	cmake ..
+	make package
+}
+
+main;
